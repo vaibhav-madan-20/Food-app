83b8926c947eb63b9bf37b22d448051063ccf918
Vaibhav Madan <vaibhavmadan15@gmail.com>
2026-02-28 22:57:04 +0530

    fix: use corsproxy.io for menu endpoint (Swiggy blocks server-side requests)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,5 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const RES_API = "/api/restaurants";
 
-export const MENU_API = "/api/menu?restaurantId=";
+const MENU_API_TARGET = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.900965&lng=75.8572758&restaurantId=";
+export const MENU_API = `https://corsproxy.io/?url=${encodeURIComponent(MENU_API_TARGET)}`;
